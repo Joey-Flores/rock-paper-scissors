9235f0b2361555a2f9ec6ecfd6c91e738b5cf75f
Joey-Flores <joeyflores98@gmail.com>
2022-08-01 19:35:47 -0700

    added path to proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  // app.use(
+  //   createProxyMiddleware(
+  //     ["/login", , "/logout", "/register", "/score", "/account"],
+  //     { target: "https://evening-shore-30877.herokuapp.com/" }
+  //   )
+  // );
   app.use(
-    createProxyMiddleware(
-      // ["/login", , "/logout", "/register", "/score", "/account"],
-      "**",
-      { target: "https://evening-shore-30877.herokuapp.com/" }
-    )
+    "/",
+    createProxyMiddleware({
+      target: "https://evening-shore-30877.herokuapp.com/",
+      changeOrigin: true,
+    })
   );
 };
