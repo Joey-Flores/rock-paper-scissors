436e62c4cd80ef0b2ff48fa6e417f80fd550ce7a
Joey-Flores <joeyflores98@gmail.com>
2022-08-01 19:27:38 -0700

    na

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(
-      ["/login", , "/logout", "/register", "/score", "/account"],
+      // ["/login", , "/logout", "/register", "/score", "/account"],
+      "**",
       { target: "https://evening-shore-30877.herokuapp.com/" }
     )
   );
